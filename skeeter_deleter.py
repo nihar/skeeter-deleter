df6ca1263b4d619c07fde163700b3bf0b3cec74c
Nihar Shah <nihar@nihar.io>
2026-01-07 12:19:25 -0800

    fix: update the replies condition to only filter stale replies

--- skeeter_deleter.py
+++ skeeter_deleter.py
@@ -138,10 +138,11 @@ class PostQualifier(models.AppBskyFeedDefs.PostView):
         Returns:
             bool: True if the post should be deleted, False otherwise.
         """
-        if (post.is_viral(viral_threshold) or post.is_stale(stale_threshold, now) or \
-                (replies_only and post.is_reply(post))) and \
+        if (post.is_viral(viral_threshold) or post.is_stale(stale_threshold, now)) and \
                 not post.is_protected_domain(domains_to_protect) and \
                 not post.is_self_liked(self_likes):
+            if  replies_only:
+                return post.is_reply(post)
             return True
         return False
 
